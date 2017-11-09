3a1e23b166bb647ed4469256f0fe9a8c804cb2ab
SatriaSuprapto <32392756+SatriaSuprapto@users.noreply.github.com>
2017-11-09 11:40:03 +0700

    algoritma yang menghitung selisih absolute dari dua nilai (X dan Y)

--- tugas1.cpp
+++ tugas1.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main () {
+	int bil1;
+	int bil2;
+	int hasil;
 	
+	cout << "masukan bilangan 1 : ";
+	cin >>bil1;
+	cout << "masukan bilangan 2 : ";
+	cin >>bil2;
+	
+	if (bil1>bil2)
+	hasil = bil1 - bil2;
+	else
+	hasil = bil2 - bil1;
+	cout << "bilangan absolute nya adalah : "<<hasil;
 }
